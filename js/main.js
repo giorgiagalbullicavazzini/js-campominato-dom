15509bd9bb3fc1b9cf018f0576902b91997738c4
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-03 15:39:39 +0100

    fix base project

--- js/main.js
+++ js/main.js
@@ -16,13 +16,13 @@ function addClass (element, className) {
     element.classList.add(className);
 }
 
-// A function to index a generic element
-function addIndex (element, i) {
-    element.innerText = i;
+// A function to add a text to an element
+function addText (element, text) {
+    element.innerText = text;
 }
 
-// A function to create a grid generator
-function gridGenerator (value) {
+// A function to save the number of rows of a grid game
+function gridRowGenerator (value) {
     let counter = 0;
 
     switch (value) {
@@ -62,7 +62,7 @@ play.addEventListener('click', function() {
     // The number of cells will be decided by recalling the proper function and by adding the correct value to the function arguments
     const difficulty = document.getElementById('difficulty').value;
 
-    const row = gridGenerator(difficulty);
+    const row = gridRowGenerator(difficulty);
     const cells = row * row;
 
     // Creation of the grid
@@ -70,7 +70,7 @@ play.addEventListener('click', function() {
         // The cells have the proper class and numbers
         const cell = elementCreation('div');
         addClass(cell, 'cell');
-        addIndex(cell, i);
+        addText(cell, i);
 
         // The number of cells in a row will be decided by a CSS class
         if (row === 10) {
@@ -90,3 +90,32 @@ play.addEventListener('click', function() {
         })
     }
 })
+
+
+
+// Regardless of the difficulty chosen by the user, the minesweeper grid will contain 16 bombs, placed in 16 random cells
+// In order to generate them, we need to create an empty array
+// The array will then be filled by a `while` loop
+// The loop will generate 16 random numbers between 1 and the number of cells of the grid
+// In order to avoid duplicate numbers, the loop will also check if the randomized number is inside the array or not
+
+
+// We can now modify the function able to make the cells clickable
+// IF the number of the cell is inside the bomb array, the cell turns red and the game is over
+// ELSE the cell turns blue and the user can keep playing
+// The user reaches the game over state if they click on a bomb cell or if they reach the maximum number of clicks (number or cells of the grid - 16 bombs)
+// The maximum number of clicks can be managed thanks to another empty array
+// This array can be filled with the numbers clicked by the user
+// IF the number isn't part of the array, we can add it with the `push` method
+// With the push, we can then manage the user score
+// For every push made in the array, the user scores 1 point
+// At the end of the game, the software generates the final score of the user
+
+
+// IF the user clicks on a bomb cell, the game is over and the grid cannot be clicked anymore
+// In order to achieve this result, we can just remove the cell EventListener
+
+
+// At the game over state, the software reveals all the bombs
+// This behaviour can be triggered with a `for` loop
+// IF the user clicks on a bomb cell, all the cells contained in the bomb array turn red
